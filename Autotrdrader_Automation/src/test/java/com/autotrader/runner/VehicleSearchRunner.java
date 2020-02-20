62f854a07c6b8bc4230038142d2c61d92eafa337
Sini-Git <61291205+Sini-Git@users.noreply.github.com>
2020-02-20 13:47:00 -0500

    Added autotrader automation Scripts

---
+++ Autotrdrader_Automation/src/test/java/com/autotrader/runner/VehicleSearchRunner.java
@@ -0,0 +1,18 @@
+package com.autotrader.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+ features = "src/test/resources/features",
+ glue = "com.autotrader.steps"
+ 
+ )
+
+public class VehicleSearchRunner {
+	
+
+}
